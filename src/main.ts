b4f9e2d5fb8db86c4420caca5654b84d33600ccb
malikbredehoeft <131286836+malikbredehoeft@users.noreply.github.com>
2025-03-27 12:39:49 +0100

    feat: add config files

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,61 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+/*
+ * Copyright (C) 2021 - present Juergen Zimmermann, Hochschule Karlsruhe
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
 
-async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
-}
-bootstrap();
+import {
+  DocumentBuilder,
+  type SwaggerCustomOptions,
+  SwaggerModule,
+} from '@nestjs/swagger';
+import { type INestApplication, ValidationPipe } from '@nestjs/common';
+import { AppModule } from './app.module.js';
+import { NestFactory } from '@nestjs/core';
+import { corsOptions } from './config/cors.js';
+import { helmetHandlers } from './security/http/helmet.handler.js';
+import compression from 'compression';
+import { nodeConfig } from './config/node.js';
+import { paths } from './config/paths.js';
+
+const { httpsOptions, port } = nodeConfig;
+
+const setupSwagger = (app: INestApplication) => {
+  const config = new DocumentBuilder()
+      .setTitle('Buch')
+      .setDescription('Beispiel für Software Engineering')
+      .setVersion('2024.04.0')
+      .addBearerAuth()
+      .build();
+  const document = SwaggerModule.createDocument(app, config);
+  const options: SwaggerCustomOptions = {
+      customSiteTitle: 'SWE 23/24',
+  };
+  SwaggerModule.setup(paths.swagger, app, document, options);
+};
+
+const bootstrap = async () => {
+  const app = await NestFactory.create(AppModule, { httpsOptions });
+  app.use(helmetHandlers, compression());
+
+  app.useGlobalPipes(new ValidationPipe());
+
+  setupSwagger(app);
+
+  app.enableCors(corsOptions);
+
+  await app.listen(port);
+};
+
+await bootstrap();
