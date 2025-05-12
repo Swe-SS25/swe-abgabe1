ea267b9a0da43e858392cd138669fc804505596b
ahmado1310 <43607545+ahmado1310@users.noreply.github.com>
2025-05-12 11:54:14 +0200

    feat: added read service

--- src/supplement/service/supplement-read.service.ts
+++ src/supplement/service/supplement-read.service.ts
@@ -1,5 +1,34 @@
 import { Injectable } from "@nestjs/common";
+import { Repository } from "typeorm";
+import { Supplement } from "../entity/supplement.entity";
+import { getLogger } from "../../logger/logger";
+import { InjectRepository } from "@nestjs/typeorm";
+
+export type FindByIDParams = {
+    readonly id: number;
+};
 
 @Injectable()
 export class SupplementReadService {
+    static readonly ID_PATTERN = /^[1-9]\d{0,10}$/u;
+
+    readonly #fileRepo: Repository<Supplement>;
+    
+    readonly #logger = getLogger(SupplementReadService.name);
+
+    constructor(
+        @InjectRepository(Supplement) fileRepo: Repository<Supplement>
+    ){
+        this.#fileRepo = fileRepo;
+    }
+
+    async findById({
+        id
+    }: FindByIDParams): Promise<Readonly<Supplement>>{
+        this.#logger.debug('findById: id=%d', id);
+
+        const supplement = await t
+
+        return supplement;
+    }
 }
