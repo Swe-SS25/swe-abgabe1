b25dc7ef762b5100c4962aefa7b7f32604df1321
malikbredehoeft <131286836+malikbredehoeft@users.noreply.github.com>
2025-06-03 15:01:25 +0200

    chore: fix linting errors

--- src/main.ts
+++ src/main.ts
@@ -1,19 +1,17 @@
-/*
- * Copyright (C) 2021 - present Juergen Zimmermann, Hochschule Karlsruhe
- *
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see <https://www.gnu.org/licenses/>.
- */
+// Copyright (C) 2021 - present Juergen Zimmermann, Hochschule Karlsruhe
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
 import {
     DocumentBuilder,
@@ -21,11 +19,11 @@ import {
     SwaggerModule,
 } from '@nestjs/swagger';
 import { type INestApplication, ValidationPipe } from '@nestjs/common';
-import { AppModule } from './app.module.js';
 import { NestFactory } from '@nestjs/core';
+import compression from 'compression';
+import { AppModule } from './app.module.js';
 import { corsOptions } from './config/cors.js';
 import { helmetHandlers } from './security/http/helmet.handler.js';
-import compression from 'compression';
 import { nodeConfig } from './config/node.js';
 import { paths } from './config/paths.js';
 
